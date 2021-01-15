d1e424a0c8c8708b55768fad22e7a29a8a761667
adam <adam-westerski@i2r.a-star.edu.sg>
2021-01-15 19:53:31 +0800

    final ver with added lane changing and project writeup

--- README.md
+++ README.md
@@ -1,5 +1,73 @@
-# CarND-Path-Planning-Project
-Self-Driving Car Engineer Nanodegree Program
+# **Path Planning** 
+
+## Udacity Project 7 Writeup
+
+---
+
+**Path Planning Project**
+
+The goals / steps of this project are the following:
+* drive a car in the virtual highway implemented in the simulator
+* car must not hit other vehicles, not exceed speed limit (50 MHP) but not drive too slow either
+* car must not exceed total accelation 10m/s^2 and not excced jerk of 10 m/s^3
+* car should be capable of changing lanes when the traffic ahead is too slow and when other lanes are empty
+* car should be able to drive min. 4.32 miles without incident.
+* car doesn't spend more than a 3 second length out side the lanes and stayed on the allowed lanes only
+
+---
+
+In comparison to course material the key code modificatoons / customizations are in "main.cpp" and related to:
+* driving car with proper speed, without crashing into other cars, without exceeding allowed accelation and jerk - I implemtned this following pretty much exectly the Udacity Q&A video tutorial for Project 7
+* lane changing - implemented based on monitoring state of all lanes (rather than FSM for own car)
+
+[//]: # (Image References)
+
+[image_1]: ./img/1hour_simulator_drive.png "Simulator testing"
+
+**Summary:** For this project I followed the extensive project Q&A video, which actually helped to implement everything in a simplistic and minimum required version for the project except of lane changing. I picked up where the Q&A left and implemented two code blocks: one for logic related to analyzing traffic for each lane and second block for decision making related to lane change. See details on each below. I found the main challenges were related to tuning the exact lane changing decision boundaries for the car (ie. akin to cost function parameters) and the randomness of the simulator making it difficult to repeat edge cases that caused failures in driving. **Testing wise, for final solution I left the car driving in simulator for 1 hour (\~43 miles) , managing to go without any incident until then and beyond.**
+
+![alt text][image_1]
+
+---
+
+## Implementation Details
+
+### Driving car on its own lane
+
+I implemented this part following the instructions from the project Q&A. My code is pretty much exactly the same as the one from Udacity. I just made some small re-arrangements and comments in a way that made more sense to me. The code responsible for determining trajectory that follows the road curvature, driving at max. allowable speed on own lane and avoiding collision is located in lines 205-314.
+
+### Lane Changing 
+
+This is the only real addition on top of what was provided. Few lines of code but actually a lot of testing and frustration. Instead of doing a Finite-state machine for the car and a cost function, I thought it would be simpler to constantly monitor the state of all three lanes and update if they are taken or not. 
+
+The code for monitoring state of lanes is located in lines 143 -165, inside the sensor_fusion loop (as implemented part of the Q&A). Firstly, I assign every object to a lane (using equation similar to one from Q&A for determining if object is on car's lane); later based on several conditions I determine if lane is taken or not: 
+* I've taken a quite conservative approach for lane changing requiring plenty of space (ie. 30 meters ahead and 20 behind)
+* another rare case that I tried to mitigate was all three lanes occupied with cars, on rare occasions that caused the vehicle to bounce from one lane to another. Here apart of presence or absence of object on lane, I check for its speed (and don't switch to lanes that are equally blocked)
+* additionally, similar as checking for object on lane and their speed ahead of the vehicle, I also do the same behind and don't allow lane change if there is a faster moving car behind
+
+The code for executing lane change is located in lines 181 -195. This part is a modified version of the speed adjustment section from Q&A - if the road ahead is taken by a slow vehicle apart of slowing down I additionally check if there is an option to change lanes. This is quite basic, except of one modification where apart of lane changing I also make sure that the car is not slowing down and accelerate when doing the lane change.
+
+## Challenges
+
+* simple bugs cause very expected behavior - my first issue was that at some seemingly random moments car tended to stop driving or behaved against the coded rules. Turned out that at very very rare occasions the sensor_fusion data contains objects that do not belong to any lane - originally I didn't account for that as a result of which my code was writing into inexistant position of a vector, probably messing up some other variable and creating very erratic behavior.
+
+* edge cases - the simulator generating traffic situation on random makes it difficult to re-create some rare situations that cause issues (crashes etc.). Sometimes I had to wait for half an hour to see the same situation re-appear again. This relates also to another problem of tracking what happened leading to a crash or some unwanted behavior. For debugging I ended up doing video screen capture with simulator window next to log window - having both in sync and to an extent capable to connecting the road situation with all data in path_planning code.
+
+* simulator performance - similar to previous Project, I find the simulator making the task more difficult rather than easy. Having the 3d view is nice but I think very unnecessary. Ideally there should be simplified 2d top view, without too much graphics. Currently, the simulator takes a lot of machine resources and (in my configuration) after running for a while, e.g. 10-20min sometimes the communication path_planner <-> simulator seems to fail. I didn't fully track the origin of the issue but it appears like some data is missing (either not passed to path_planner code or packets lost). 
+
+## Possible improvements
+
+* The current speed adjustment is very basic. The most annoying part is when there is a slow moving car ahead and no option to change lane, our vehicle is slowing down and speeding up like a yo-yo. I spent some time trying to mitigate this but eventually I didn't manage to get it working properly therefore did not include in the solution.
+
+* Finite-state machine for vehicle and explicit cost function. My solution of tracking lane states and implicitly weighting cost of lane changing via various conditions is quite simplistic, I thinking to try to the FSM but as I was running out of time until deadline, so I decided to skip it. Adjusting the cost function would undoubtably take more time).
+
+--- 
+
+## Project Execution Info (by Udacity) 
+
+### Goals
+
+In this project the goal is to safely navigate around a virtual highway with other traffic that is driving +-10 MPH of the 50 MPH speed limit. Car's localization and sensor fusion data are provided, there is also a sparse map list of waypoints around the highway. The car should try to go as close as possible to the 50 MPH speed limit, which means passing slower traffic when possible, note that other cars will try to change lanes too. The car should avoid hitting other cars at all cost as well as driving inside of the marked road lanes at all times, unless going from one lane to another. The car should be able to make one complete loop around the 6946m highway. Since the car is trying to go 50 MPH, it should take a little over 5 minutes to complete 1 loop. Also the car should not experience total acceleration over 10 m/s^2 and jerk that is greater than 10 m/s^3.
    
 ### Simulator.
 You can download the Term3 Simulator which contains the Path Planning Project from the [releases tab (https://github.com/udacity/self-driving-car-sim/releases/tag/T3_v1.2).  
@@ -9,8 +77,6 @@ To run the simulator on Mac/Linux, first make the binary file executable with th
 sudo chmod u+x {simulator_file_name}
 ```
 
-### Goals
-In this project your goal is to safely navigate around a virtual highway with other traffic that is driving +-10 MPH of the 50 MPH speed limit. You will be provided the car's localization and sensor fusion data, there is also a sparse map list of waypoints around the highway. The car should try to go as close as possible to the 50 MPH speed limit, which means passing slower traffic when possible, note that other cars will try to change lanes too. The car should avoid hitting other cars at all cost as well as driving inside of the marked road lanes at all times, unless going from one lane to another. The car should be able to make one complete loop around the 6946m highway. Since the car is trying to go 50 MPH, it should take a little over 5 minutes to complete 1 loop. Also the car should not experience total acceleration over 10 m/s^2 and jerk that is greater than 10 m/s^3.
 
 #### The map of the highway is in data/highway_map.txt
 Each waypoint in the list contains  [x,y,s,dx,dy] values. x and y are the waypoint's map coordinate position, the s value is the distance along the road to get to that waypoint in meters, the dx and dy values define the unit normal vector pointing outward of the highway loop.
@@ -139,7 +205,4 @@ that's just a guess.
 
 One last note here: regardless of the IDE used, every submitted project must
 still be compilable with cmake and make./
-
-## How to write a README
-A well written README file can enhance your project and portfolio.  Develop your abilities to create professional README files by completing [this free course](https://www.udacity.com/course/writing-readmes--ud777).
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -49,9 +50,14 @@ int main() {
     map_waypoints_dx.push_back(d_x);
     map_waypoints_dy.push_back(d_y);
   }
+    
+  // Start in lane 1
+  int lane = 1;
 
+  // Have a reference velocity to target
+  double ref_vel = 0.0; //49.5; // mph
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+               &map_waypoints_dx,&map_waypoints_dy, &lane, &ref_vel]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -88,16 +94,242 @@ int main() {
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
 
-          json msgJson;
+            /**
+             * TODO: define a path made up of (x,y) points that the car will visit
+             *   sequentially every .02 seconds
+             */
 
+            
+          // Provided previous path point size
+          int prev_size = previous_path_x.size();
+            
+          //list of points in x-y coord system to add into the path (this will be used for interpolating spline)
+          vector<double> ptsx;
+          vector<double> ptsy;
+            
+          // Reference x, y, and yaw states
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+            
+            
+          // account for cars on the road
+          if(prev_size > 0) //car starting position is last of the path
+          {
+              car_s = end_path_s;
+          }
+
+          bool too_close = false;
+            double front_car_speed = -1;
+          vector<bool> lane_taken = {false, false, false}; //for each lane keep flag if its safe to change or not
+          // iterate over all objects on the road (sensor_fusion data) and check for possible collisions
+            
+            for (int i = 0; i < sensor_fusion.size(); i++) {
+                float check_object_d = sensor_fusion[i][6]; // d value = tell object position horizontally (ie. on which lane)
+                double check_object_s = sensor_fusion[i][5]; //s value = tell object position vertically (ie. how far along the road, used to check if object position is ahead / behind our car)
+                //check the speed on the object
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx+vy*vy);
+
+                // dummy way to estimate objects position in future = assume will keep same speed and multiply by amount of time stamps similar as for our own car (ie. one simulator time stamp is 0.02s)
+                // = 0.02 * (our own car prediction length from previous iteration) * current_object_speed
+                check_object_s += ((double)prev_size*.02*check_speed);
+                
+                //object lane
+                int check_lane = -1;
+                //calculate object lane based on d value
+
+                for (int lane_no = 0; lane_no < 3; lane_no++) // iterate 3 possible lanes = {0,1,2}
+                {
+                    if ( check_object_d< (2+4*lane_no+2) && check_object_d > (2+4*lane_no-2) ) {
+                        check_lane = lane_no;
+                    }
+                }
+                
+                if (check_lane <0) { //some reported objects in sensor_fusion apparently can be outside of the three lanes, igonore them
+                    continue;
+                }
+                
+                //check which lanes are taken
+                if (   (car_s - check_object_s  < 30)
+                      && (check_object_s - car_s < 30 ))     //if object is 30meters behind or less OR object is 30 meters ahead or less
+                {
+                    lane_taken[check_lane] = true;
+                } else if ( (check_object_s - car_s < 90) &&  (check_speed > car_speed) ) { //if object ahead has lower speed dont change into its lane
+                    lane_taken[check_lane] = true;
+                } else if ( ( car_s - check_object_s  > 30) //if car 30 meters behind me and has greater speed
+                    && (check_speed > car_speed) )
+                {
+                    lane_taken[check_lane] = true;
+                }
+
+                if ( check_object_d< (2+4*lane+2) && check_object_d > (2+4*lane-2) ) { //check if object on same lane as our car
+                    
+                    if ( (check_object_s - car_s > 0) && (check_object_s - car_s < 30) ) //if (1) object is ahead of our car; AND (2) if the gap is smaller than 30meters
+                    {
+                        too_close = true;
+                        front_car_speed = check_speed;
+                    }
+                }
+            }
+
+          // Control car speed and execute lane changing
+          if (too_close)
+          { //slow down
+              //consider lane changing
+              if ( (lane -1 >= 0) && (lane_taken[lane -1] == false) ) { //change left
+                  lane = lane -1;
+                  if (ref_vel < 49.5) // Speed up
+                  {
+                      ref_vel += 0.224;
+                  }
+              } else if ( (lane +1 <= 2) && (lane_taken[lane +1] == false) ) { //change right
+                  lane = lane +1;
+                  if (ref_vel < 49.5) // Speed up
+                  {
+                      ref_vel += 0.224;
+                  }
+              } else {
+                  ref_vel -= 0.224;
+              }
+          }
+          else if (ref_vel < 49.5) // Speed up
+          {
+              ref_vel += 0.224;
+          }
+            
+            
+
+          //1: create spline along the road curvature (in xy coord system) and sample points from it
+          //1.1: For approx. trajectory begining (ie. spline start) == add two starting points related to car history (ie. previous path or if doesn't exist car position)
+            
+          //(previous path is returned by the simulator and accoutns for the path passed on by this trajectory planned in prior iteration, minus the waypoints that
+          // were already crossed by the car as time passed from prior iteration)
+            
+          // If previous size is almost empty, use the car as a starting reference
+          if (prev_size < 2)
+          {
+              // Use two points that make the path tangent to the car
+              double prev_car_x = car_x - cos(car_yaw);
+              double prev_car_y = car_y - sin(car_yaw);
+
+              ptsx.push_back(prev_car_x);
+              ptsx.push_back(car_x);
+
+              ptsy.push_back(prev_car_y);
+              ptsy.push_back(car_y);
+          } else //if more than two points in the previous path == use last two points and add it for charting new path
+          {
+              // Redefine reference state as previous path end point
+              ref_x = previous_path_x[prev_size - 1];
+              ref_y = previous_path_y[prev_size - 1];
+
+              double ref_x_prev = previous_path_x[prev_size - 2];
+              double ref_y_prev = previous_path_y[prev_size - 2];
+              ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+              // Use two points that make the path tangent to the previous path's end point
+              ptsx.push_back(ref_x_prev);
+              ptsx.push_back(ref_x);
+
+              ptsy.push_back(ref_y_prev);
+              ptsy.push_back(ref_y);
+          }
+            
+          //1.2: Add on 3 extra points moving ahead from the current car position. (use Frenet car coords as starting point and add three points with even 30m spacing)
+          // s = add in the 30m spacing
+          // d = 4meters * lane_number + 2meters ( 2meters so the car is in the center of the lane)
+          vector<double> next_wp0 = getXY(car_s + 30, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s + 60, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s + 90, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+            
+          //1.3: additional "trick" - move from map coordinates to local car coordinates (this way inital spline position is zero, instead of some funky number changine in time)
+          // (this makes it easier to samples points from the spline)
+          for (int i = 0; i < ptsx.size(); i++)
+          {
+              // Shift car reference angle to 0 degrees
+              double shift_x = ptsx[i] - ref_x;
+              double shift_y = ptsy[i] - ref_y;
+
+              ptsx[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
+              ptsy[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
+          }
+            
+          //1.4: Create a spline that interpolates points calculated in 1.1 and 1.2
+          tk::spline trajectory_spline;
+          // Set (x,y) points to the spline
+          trajectory_spline.set_points(ptsx, ptsy);
+            
+          //1.5: calculate the final points of the trajectory (take previous path with not yet "used up" points and add on points sampled from the spline)
+
+          // Define the actual (x, y) points we will use for the planner
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
+          //1.5.1: Start with all of the previous path points from last time
+          for (int i = 0; i < prev_size; i++)
+          {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
 
+          //1.5.2: Add in points from the spline
+          //First - Calculate how to break up spline into points so that we travel at our desired reference velocity
+          double target_x = 30.0; //ending distance from the current position (meters)
+          double target_y = trajectory_spline(target_x);
+          double target_dist = sqrt((target_x)*(target_x)+(target_y)*(target_y));
+
+          double x_add_on = 0;
+
+          // Fill up the rest of our path planner after filling it with previous points, here we will always output 50 points
+          for(int i = 1; i < 50 - prev_size; i++)
+          {
+              double N = target_dist/(0.02*ref_vel/2.24); //2,24 == conversion from miles to km, 0.02 == time step used in similator for sampling points
+              double x_point = x_add_on + target_x/N;
+              double y_point = trajectory_spline(x_point);
+
+              x_add_on = x_point;
+
+              double x_ref = x_point;
+              double y_ref = y_point;
+
+              // Rotate back to normal map coordinates from local car coordinates after rotating it earlier
+              x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+              y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+              x_point += ref_x;
+              y_point += ref_y;
+
+              next_x_vals.push_back(x_point);
+              next_y_vals.push_back(y_point);
+          }
+            
+          json msgJson;
+
+//
+//            //APPROACH 1: move car in fixed distance steps along the road curvature
+//            // Define the actual (x, y) points we will use for the planner
+//            vector<double> next_x_vals;
+//            vector<double> next_y_vals;
+//
+//            double dist_inc = 0.5;
+//            for (int i = 0; i < 50; ++i) { //plan trajectory for 50 steps ahead
+//                double next_s = car_s+(i+1)*dist_inc; //current car position + some fixed distance increment
+//                double next_d = 6; //constant for middle lane, 0 +4 (4 = lane width) , + 2 = middle of the (middle/centre) lane
+//                //convert to x,y coordinates as those need to passed to JSON
+//                vector<double> xy = getXY(next_s,next_d,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+//                next_x_vals.push_back(xy[0]);
+//                next_y_vals.push_back(xy[1]);
+//            }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
